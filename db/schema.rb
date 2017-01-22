0e976cfd33b72f77f8df580c1efb05dfeaf6e70b
akadane87 <akadane87@gmail.com>
2017-01-22 15:15:25 -0500

    batch import/seed data
    wrote a script for importing names and pics with
    correct cooresponding ids to avoid mix ups.
    ORDER MATTERS when importing the rows in the script.
    
    Couldnt find a way to import the columns simulateously so the solution
    that worked was splitting them into 2 seperate csv files. The data is
    seeded correctly in the corresponing tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20170113221907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "image"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
@@ -24,6 +31,32 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   add_index "examples", ["user_id"], name: "index_examples_on_user_id", using: :btree
+
+  create_table "names", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "names", ["picture_id"], name: "index_names_on_picture_id", using: :btree
+
+  create_table "pictures", force: :cascade do |t|
+    t.integer  "name_id",    null: false
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pictures", ["name_id"], name: "index_pictures_on_name_id", using: :btree
+
+  create_table "responses", force: :cascade do |t|
+    t.boolean  "response"
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -37,4 +70,5 @@ ActiveRecord::Schema.define(version: 2) do
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
   add_foreign_key "examples", "users"
+  add_foreign_key "pictures", "names"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database
 # with its default values.
@@ -8,3 +10,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+
+CSV.foreach('./names.csv', :headers => true) do |row|
+  Name.create!(row.to_hash)
+end
+
+CSV.foreach('./pictures.csv', :headers => true) do |row|
+  Picture.create!(row.to_hash)
+end
