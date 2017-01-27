a603618f2f4d523c7d1b4e36f18fee3566fbc4a6
akadane87 <akadane87@gmail.com>
2017-01-27 04:10:02 -0500

    commenting out remove Attempts for heroku fix

--- db/migrate/20170126193859_remove_attempts_from_users.rb
+++ db/migrate/20170126193859_remove_attempts_from_users.rb
@@ -1,7 +1,7 @@
-class RemoveAttemptsFromUsers < ActiveRecord::Migration
+# class RemoveAttemptsFromUsers < ActiveRecord::Migration
   # def change
     # remove_column :users, :Attempts, :integer
     # remove_column :users, :attempt, :integer
     # remove_column :users, :attempts, :integer
   # end
-end
+# end
