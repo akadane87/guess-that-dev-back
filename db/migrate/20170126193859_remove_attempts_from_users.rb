b6ba1ef4c35904853b2941c76520e6d98108159b
akadane87 <akadane87@gmail.com>
2017-01-27 04:05:19 -0500

    attmept to fix heroku error

--- db/migrate/20170126193859_remove_attempts_from_users.rb
+++ db/migrate/20170126193859_remove_attempts_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveAttemptsFromUsers < ActiveRecord::Migration
-  def change
+  # def change
     # remove_column :users, :Attempts, :integer
-    remove_column :users, :attempt, :integer
-    remove_column :users, :attempts, :integer
-  end
+    # remove_column :users, :attempt, :integer
+    # remove_column :users, :attempts, :integer
+  # end
 end
