4ad19888b8e06571db26db989b9337f6763d1b56
akadane87 <akadane87@gmail.com>
2017-01-27 04:00:36 -0500

    comment out remove Attempts colum for heroku
    also active record ready seed file

--- db/migrate/20170126193859_remove_attempts_from_users.rb
+++ db/migrate/20170126193859_remove_attempts_from_users.rb
@@ -1,6 +1,6 @@
 class RemoveAttemptsFromUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :Attempts, :integer
+    # remove_column :users, :Attempts, :integer
     remove_column :users, :attempt, :integer
     remove_column :users, :attempts, :integer
   end
